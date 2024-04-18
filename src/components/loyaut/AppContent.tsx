da2508a541881466e3e82e168b38125aa2f5f7ae
ReDaX7355 <aleks.megalodon@gmail.com>
2024-04-18 19:38:26 +0300

    Обработка запроса крипты

--- src/components/loyaut/AppContent.tsx
+++ src/components/loyaut/AppContent.tsx
@@ -1,33 +1,32 @@
-import { Layout, Typography } from 'antd';
-import { FC } from 'react';
-import { useSelector } from 'react-redux';
-import { RootState } from '../../context/store';
-import PortfolioChart from '../PortfolioChart';
-import AssetsTable from '../AssetsTable';
+import { Layout, Typography } from "antd";
+import { FC } from "react";
+import { useSelector } from "react-redux";
+import { RootState } from "../../context/store";
+import PortfolioChart from "../PortfolioChart";
+import AssetsTable from "../AssetsTable";
 
 const contentStyle: React.CSSProperties = {
-  padding: '1rem',
-  minHeight: 'calc(100vh - 60px)',
-  color: '#fff',
-  backgroundColor: '#001529',
+  padding: "1rem",
+  minHeight: "calc(100vh - 60px)",
+  color: "#fff",
+  backgroundColor: "#001529",
 };
 
 const AppContent: FC = () => {
   const { assets, crypto } = useSelector((state: RootState) => state);
 
-  const cryptoPriceMap = crypto.data.reduce(
-    (acc: { [key: string]: number }, coin) => {
+  const cryptoPriceMap =
+    crypto.data &&
+    crypto.data.reduce((acc: { [key: string]: number }, coin) => {
       acc[coin.id] = coin.price;
       return acc;
-    },
-    {}
-  );
+    }, {});
 
   return (
     <Layout.Content style={contentStyle} className="content">
       {assets.data.length != 0 && (
-        <Typography.Title level={3} style={{ color: '#fff' }}>
-          Portfolio:{' '}
+        <Typography.Title level={3} style={{ color: "#fff" }}>
+          Portfolio:{" "}
           {assets.data
             .map((asset) => {
               return asset.amount * cryptoPriceMap[asset.id_coin];
